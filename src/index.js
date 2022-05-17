dd0e9531e3893c367cc022447d8a380d5a21a4e7
serdarov567 <serdaroff567@gmail.com>
2022-05-17 15:34:35 +0500

    firebase update

--- src/index.js
+++ src/index.js
@@ -5,6 +5,27 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./theme";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBP5DWISpiiaZk3ld0yDyI7FDtZlYcaH_M",
+  authDomain: "cryptowaveclub.firebaseapp.com",
+  projectId: "cryptowaveclub",
+  storageBucket: "cryptowaveclub.appspot.com",
+  messagingSenderId: "529700855210",
+  appId: "1:529700855210:web:34f8666ea27e6da820dfd3",
+  measurementId: "G-L1V9FTCS9G",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
